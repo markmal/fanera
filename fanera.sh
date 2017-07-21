1d0e70a89a29dc47be07b74f284ddbe3e39ab6b9
markmal <markmal@github.com>
2017-07-21 09:36:34 -0400

    added ant task for building released packages (tar for Linux and zip for Windows)

---
+++ fanera.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#JAVA=$(which java)
+JAVA=~/opt/jdk1.8.0_131/bin/java
+$JAVA -version 2>&1| grep -i '64-Bit' >/dev/null 2>&1
+if [ $? -eq 0 ]; then ARCH=amd64; else ARCH=i386; fi
+
+CP=
+for J in lib/*.jar; do CP=$CP:$J; done 
+
+LD_LIBRARY_PATH=lib/$ARCH $JAVA -jar fanera.jar $*
